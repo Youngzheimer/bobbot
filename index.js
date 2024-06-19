3e37b0bb2176b2bfa55c98542e5cb95c56c3c686
Youngzheimer <youngzheimer@gmail.com>
2024-06-19 15:57:40 +0900

    school search

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const discord = require("discord.js");
 const config = require("./config.json");
 const moment = require("moment");
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./schoolList.db');
+
+db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, sccode TEXT, schoolcode TEXT, schoolname TEXT)', (err) => {
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('테이블 users가 성공적으로 생성되었습니다.');
+    }
+});
+
 async function getbob(date, school, sc) {
     const bob = await axios.get(config.bobapi.url, {
         params: {
@@ -35,6 +46,20 @@ function makeBobEmbed(bob, day = moment().format('YYYYMMDD')) {
     return embed;
 }
 
+async function searchSchool(query) {
+    const school = await axios.get(config.bobapi.searchurl, {
+        params: {
+            KEY: config.bobapi.apikey,
+            Type: "json",
+            pIndex: 1,
+            pSize: 100,
+            SCHUL_NM: query
+        }
+    });
+
+    return school.data.schoolInfo;
+}
+
 const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds, discord.GatewayIntentBits.GuildMessages, discord.GatewayIntentBits.DirectMessages, discord.GatewayIntentBits.MessageContent], partials: [discord.PartialTextBasedChannel, discord.PartialGroupDMChannel] });
 
 client.once(discord.Events.ClientReady, readyClient => {
@@ -62,6 +87,77 @@ client.on(discord.Events.MessageCreate, async message => {
         });
         console.log(acbob)
     }
+
+    if (message.content.startsWith("!school")) {
+        var command = message.content.split(" ")
+
+        if (command[1] === undefined) {
+            db.get('SELECT * FROM users WHERE id = ?', [message.member.id], async (err, row) => {
+                if (!row) {
+                    message.reply("검색어를 입력해주세요.")
+                    return;
+                }
+
+                message.reply(`당신의 학교는 ${row.schoolname}입니다.`);
+            });
+
+            return;
+        }
+
+        searchSchool(command[1]).then(res => {
+            if (res === undefined) {
+                message.reply("검색 결과가 없습니다.")
+                return
+            }
+
+            if (res[1].row.length > 25) {
+                message.reply("검색 결과가 너무 많습니다. 더 자세하게 검색해주세요.")
+                return
+            }
+
+            // make button using interactions
+            const select = new discord.StringSelectMenuBuilder()
+                .setCustomId('schoolSelect')
+                .setPlaceholder('학교를 선택해주세요.')
+
+            res[1].row.forEach(school => {
+                select.addOptions(
+                    new discord.StringSelectMenuOptionBuilder()
+                        .setLabel(school.SCHUL_NM)
+                        .setDescription(school.ORG_RDNMA)
+                        .setValue(school.ATPT_OFCDC_SC_CODE + " " + school.SD_SCHUL_CODE + " " + school.SCHUL_NM)
+                )
+            });
+
+            const row = new discord.ActionRowBuilder()
+                .addComponents(select)
+
+            console.log(row.components)
+
+            message.reply({
+                content: '학교를 선택해주세요.',
+                components: [row]
+            });
+        });
+    }
 });
+
+client.on(discord.Events.InteractionCreate, interaction => {
+    if (!interaction.isStringSelectMenu()) return;
+
+    if (interaction.customId === 'schoolSelect') {
+        const school = interaction.values[0].split(" ");
+
+        db.run('INSERT OR REPLACE INTO users (id, sccode, schoolcode, schoolname) VALUES (?, ?, ?, ?)', [interaction.user.id, school[0], school[1], school[2]], (err) => {
+            if (err) {
+                console.error(err.message);
+                interaction.reply("학교를 설정하는 중 오류가 발생했습니다.")
+            } else {
+                interaction.reply(`학교가 설정되었습니다. (${school[2]})`);
+            }
+        });
+    }
+});
+
 
 client.login(config.discordapi.token);

1ff2d7fa7e5a48381d58afcdc5370ce178f8016f
Youngzheimer <youngzheimer@gmail.com>
2024-06-19 10:58:29 +0900

    bob API function

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const discord = require("discord.js");
 const config = require("./config.json");
+const axios = require("axios");
 
 const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds, discord.GatewayIntentBits.GuildMessages, discord.GatewayIntentBits.DirectMessages, discord.GatewayIntentBits.MessageContent], partials: [discord.PartialTextBasedChannel, discord.PartialGroupDMChannel] });
+
+async function getbob(date, school, sc) {
+    const bob = await axios.get(config.bobapi.url, {
+        params: {
+            KEY: config.bobapi.apikey,
+            Type: "json",
+            pIndex: 1,
+            pSize: 5,
+            ATPT_OFCDC_SC_CODE: sc,
+            SD_SCHUL_CODE: school,
+            MLSV_YMD: date
+        }
+    });
+
+    console.log(bob)
+
+    return bob.data.mealServiceDietInfo;
+}
+
 
 client.once(discord.Events.ClientReady, readyClient => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
